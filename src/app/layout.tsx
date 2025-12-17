e298233a84ac2ce293fff22d7cc54ee61eec0229
Tiago Savioli <tiagoxaviersavioli@gmail.com>
2025-12-17 13:52:41 -0300

    Fix favicon config

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,39 +1,40 @@
 import "./globals.css";
 
-import { Footer } from "@/sections/footer";
-import { Header } from "@/sections/header";
 import type { Metadata } from "next";
 import { Providers } from "./providers";
-import { Screen } from "@/components/screen";
 
 export const metadata: Metadata = {
-  title: "Circle App",
-  description: "Social app to create moments and share your memories",
-  icons: {
-    icon: "/favicon.ico",
-  },
+    title: "Circle App",
+    description: "Social app to create moments and share your memories",
+    icons: {
+        icon: [
+            { url: "/favicon.ico", type: "image/x-icon" },
+            { url: "/favicon.png", type: "image/png" },
+        ],
+        shortcut: ["/favicon.ico"],
+    },
 };
 
 export default function RootLayout({
-  children,
+    children,
 }: {
-  children: React.ReactNode;
+    children: React.ReactNode;
 }) {
-  return (
-    <html lang="pt-BR">
-      <body
-        style={{
-          margin: 0,
-          background: `linear-gradient(to bottom, #101010 0%, #000000 33%, #000000 100%)`,
-          minHeight: "100vh",
-          userSelect: "none",
-          WebkitUserSelect: "none",
-          MozUserSelect: "none",
-          msUserSelect: "none",
-        }}
-      >
-        <Providers>{children}</Providers>
-      </body>
-    </html>
-  );
+    return (
+        <html lang="pt-BR">
+            <body
+                style={{
+                    margin: 0,
+                    background: `linear-gradient(to bottom, #101010 0%, #000000 33%, #000000 100%)`,
+                    minHeight: "100vh",
+                    userSelect: "none",
+                    WebkitUserSelect: "none",
+                    MozUserSelect: "none",
+                    msUserSelect: "none",
+                }}
+            >
+                <Providers>{children}</Providers>
+            </body>
+        </html>
+    );
 }
